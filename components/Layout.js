356d89fcbc36e21b7f506279fb6abfcaeb189d5b
Jori Lallo <jtlallo@gmail.com>
2017-01-01 22:09:45 -0800

    Fixed SSR with styled-components

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,6 @@
-import Head from 'next/head';
 import lodash from 'lodash';
 import { Flex } from 'reflexbox';
 import { injectGlobal } from 'styled-components';
-import styleSheet from 'styled-components/lib/models/StyleSheet';
-import FontFaceObserver from 'fontfaceobserver';
 
 import Header from './Header';
 
@@ -14,52 +11,27 @@ if (global.window) {
   window._ = lodash; // eslint-disable-line
 }
 
-export default class extends React.Component {
-  componentDidMount = async () => {
-    await new FontFaceObserver('Source Code Pro').load();
-    document.documentElement.className += ' fonts-loaded';
-  }
-
-  render() {
-    const css = this.props.isServer ? styleSheet.rules().map(rule => rule.cssText).join('\n') : '';
-
-    return (
-      <Flex auto column style={{ width: '100%' }}>
-        <Head>
-          <title>json.browse</title>
-          <meta name="description" content="Browse, filter and manipulate your JSON inside the browser" />
-          <link rel="icon" type="image/png" href="static/favicon.png" />
-          <style dangerouslySetInnerHTML={{ __html: css }} />
-
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-          <meta property="og:title" content="json.browse" />
-          <meta property="og:description" content="Browse, filter and manipulate your JSON inside the browser" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://jsonbrowse.com" />
-          <meta property="og:image" content="static/images/jsonbrowse.png" />
-        </Head>
-        <Header { ...this.props } />
-        <Flex auto column style={{ overflow: 'scroll' }}>
-          { this.props.children }
-        </Flex>
-      </Flex>
-    );
-  }
-};
+export default props => (
+  <Flex auto column style={{ width: '100%' }}>
+    <Header { ...props } />
+    <Flex auto column style={{ overflow: 'scroll' }}>
+      { props.children }
+    </Flex>
+  </Flex>
+);
 
 injectGlobal`
 body, html, #__next {
   margin: 0;
   height: 100%;
 
-  font-family: 'Courier', monospace;
+  font-family: 'Source Code Pro', 'Courier', monospace;
   font-size: 16px;
   font-weight: 400;
 }
 
 input, textarea, select, button {
-  font-family: 'Courier', monospace;
+  font-family: 'Source Code Pro', 'Courier', monospace;
 }
 
 .fonts-loaded body,
---
+++ pages/_document.js
@@ -0,0 +1,42 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import styleSheet from 'styled-components/lib/models/StyleSheet';
+
+export default class MyDocument extends Document {
+  static async getInitialProps ({ renderPage }) {
+    const page = renderPage();
+    const style = styleSheet.rules().map(rule => rule.cssText).join('\n');
+    return { ...page, style };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>json.browse</title>
+          <style dangerouslySetInnerHTML={{ __html: this.props.style }} />
+
+          <meta
+            name="description"
+            content="Browse, filter and manipulate your JSON inside the browser"
+          />
+          <link rel="icon" type="image/png" href="static/favicon.png" />
+
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <meta property="og:title" content="json.browse" />
+          <meta
+            property="og:description"
+            content="Browse, filter and manipulate your JSON inside the browser"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://jsonbrowse.com" />
+          <meta property="og:image" content="static/images/jsonbrowse.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
